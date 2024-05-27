61e2385ac457120eb5bb95205d5432085188fb1b
Anurag00321 <sanurag4518@gmail.com>
2024-05-27 15:09:48 +0530

    gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -12,12 +12,6 @@ const nextConfig = {
   },
   
 };
-module.exports = {
-  env: {
-    GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
-    GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-  },
-};
 
 
 export default nextConfig;
