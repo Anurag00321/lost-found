1bb8fe740a58e936eb7f97b8587dcd785211204e
Anurag00321 <sanurag4518@gmail.com>
2024-05-27 15:07:04 +0530

    gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,16 @@ const nextConfig = {
       "res.cloudinary.com",
       "images.unsplash.com"
     ],
+    
+  },
+  
+};
+module.exports = {
+  env: {
+    GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+    GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
   },
 };
+
 
 export default nextConfig;
