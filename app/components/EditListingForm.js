640e12648ea7ed401d37ae5b190c2eb2c4c3f341
Anurag <sanurag4518@gmail.com>
2024-05-29 11:09:35 +0530

    image

--- app/components/EditListingForm.js
+++ app/components/EditListingForm.js
@@ -122,10 +122,14 @@ initValues();
                         <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                     </svg>
                     </div>
-                    {imageUrl && !publicId && <Image src={imageUrl} fill className='absolute object-cover inset-0' alt={title} />}
-            {!imageUrl && publicId && <Image src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/${publicId}`} fill className="absolute object-cover inset-0" alt={title} />}
-            {imageUrl && publicId && <Image src={filename} fill className='absolute object-cover inset-0' alt={title} />}
-
+                    {imageUrl && (
+                        <Image
+                            src={imageUrl}
+                            className="absolute object-cover inset-0"
+                            fill
+                            alt={title}
+                        />
+                    )}
                 </CldUploadButton>
                 {imageUrl && <button onClick={removeImage} className='bg-red-500 py-2 px-4 rounded-lg font-bold text-white'>Remove Image</button>}
 
